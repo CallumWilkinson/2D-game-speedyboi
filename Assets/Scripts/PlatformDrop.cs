26773648a8a21e5f8021da7f84d6486245e67d3a
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-03-29 15:20:52 +1100

    can no longer drop through base platform
    added collision detection and Boolean for objects other than base platform so you cant fall through the earth

--- Assets/Scripts/PlatformDrop.cs
+++ Assets/Scripts/PlatformDrop.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class PlatformDrop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-        
+    private bool canFallThrough = false;
     
 
     // Update is called once per frame
     void Update()
     { 
-        if (Input.GetKeyDown(KeyCode.S) || (Input.GetKeyDown(KeyCode.DownArrow)))
+        if (canFallThrough == true && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
         {
             StartCoroutine(FallTimer());
+            canFallThrough = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //when colliding with an object OTHER THAN the base platform, player can fall through
+        if (!collision.gameObject.CompareTag("BasePlatform"))
+        {
+            canFallThrough=true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        //when leaving a collision with an object OTHER THAN the base plaform, reset to false
+        if (!collision.gameObject.CompareTag("BasePlatform"))
+        {
+            canFallThrough = false;
         }
     }
 
