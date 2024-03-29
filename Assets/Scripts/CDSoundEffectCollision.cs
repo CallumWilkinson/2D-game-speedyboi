adb73a0ee0064a8267314c1f0d6e4a2abbaebd0b
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-03-29 23:14:30 +1100

    fixed james' reccomendation
    SFX now used system threading tasks rather than async await. Function is still the same it just reads alot better

--- Assets/Scripts/CDSoundEffectCollision.cs
+++ Assets/Scripts/CDSoundEffectCollision.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Security;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -8,8 +9,7 @@ public class CDSoundEffectCollision : MonoBehaviour
 {
     private AudioSource audioSource;
     public int cooldownTime = 500;
-    private bool onCooldown = false;
-    private Task _timer = Task.CompletedTask;
+    private Task timer = Task.CompletedTask;
 
 
     private void Start()
@@ -18,17 +18,13 @@ public class CDSoundEffectCollision : MonoBehaviour
     }
 
     //call when another object's collider component makes contact with this object's collider component
-    async void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         //check if player collides and play sound
-        if (_timer.IsCompleted && collision.gameObject.CompareTag("Player"))
+        if (timer.IsCompleted && collision.gameObject.CompareTag("Player"))
         {
             audioSource.Play();
-            onCooldown = true;
-            _timer = Task.Delay(cooldownTime);
-            _timer.Start();
-            //await Task.Delay((int)(cooldownTime *1000));
-            //onCooldown = false;
+            timer = Task.Delay(cooldownTime);
         }
         
 
